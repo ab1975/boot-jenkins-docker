0aad882d9b225bf541c698eba4fbc32094bf2664
ab1975 <ajaykbhosle@gmail.com>
2020-01-14 15:00:45 +0530

    First Commit

---
+++ src/main/java/com/bhosle/ajay/boot/jenkins/docker/BootJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.bhosle.ajay.boot.jenkins.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJenkinsDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhosle/ajay/boot/jenkins/docker/BootJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhosle.ajay.boot.jenkins.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
